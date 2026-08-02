22aea8808466015d9b5e1b2561020e1af6970221
WilliamJosueMP <elhermanowilliam@gmail.com>
2026-08-02 14:50:50 -0500

    Resolucion de ProblemaNuevo

---
+++ ProblemaNuevo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1;
+    int num2;
+    cout<<"Ingrese el valor  de num1: ";
+    cin>>num1
+    cout<<"Ingrese el valor  de num2: ";
+    cin>>num2
+    if(num1%2==0 && num2%2==0){
+                 cout<<"Tanto num1 como num2 son multiplos de 2"<<endl;
+                 }
+                 else {
+                      cout<<"Tanto num1 como num2 no son multiplos de 2"<<endl;
+                      }
+    return 0;

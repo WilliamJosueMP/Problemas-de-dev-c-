db9b5774eeef77c6b7eb6a90e852b14b60d45d3c
WilliamJosueMP <elhermanowilliam@gmail.com>
2026-08-02 15:04:41 -0500

    Resolucion del Bucle

---
+++ PrimerBucle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    for(int i=1; i<=5; i++){
+            string mensaje;
+            cout<<"Hola a todos: "<<mensaje<<endl;
+            }
+    return 0;     
+}

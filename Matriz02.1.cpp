b674a85deb658f4c3e7c87820f37986dd8a30bc7
WilliamJosueMP <elhermanowilliam@gmail.com>
2026-08-03 10:12:15 -0500

    Resolucion de Matriz02.1

---
+++ Matriz02.1.cpp
@@ -0,0 +1,24 @@
+    #include <iostream>
+    using namespace std;
+    int main(){
+    int n, m;
+    cout<<"Agrega el numero de elemnetos de la fila: ";
+    cin>>n;
+    cout<<"Agrega el numero de elementos de la columna: ";
+    cin>>m;
+    int Id[100][100];
+    for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                    cout<<"Coloca el valor de Id["<<i<<"]["<<j<<"] ";
+                    cin>>Id[i][j];
+                    }
+                }
+    cout<<"\nLa Matriz Principal es: "<<endl;
+    for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                    cout<<Id[i][j]<<" ";
+                    }
+            cout<<endl;
+            }
+    return 0;
+}
